11c23134c28f7f5a68c5c2becc90c1216d00f6b8
Peshal Singh Oli <peshal@Peshals-MBP.home>
2018-09-15 10:19:55 -0400

    Initial commit

---
+++ main.py
@@ -0,0 +1,30 @@
+
+
+print ("hello world") # this is for printin
+
+#This is a comment
+
+''' this is
+a
+multiline 
+commmnet
+
+
+'''
+variable = 5
+
+print(variable)
+
+#data types =5
+'''
+1.number, 2 strings, 3 lists, 4 tuples 5 dictionaries 
+ 7 operators: +,_,*,/,%, **,//
+'''
+print("lets print 3+2", 3+2)
+
+##Operator precedence
+
+string1 = "This is  \""
+print(string1)
+
+
